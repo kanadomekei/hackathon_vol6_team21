c3b122122a8864cc36820cef47e64b726bda0a97
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-02 13:14:06 +0900

    データベースへの接続の設定を変更

--- back/src/database/database.py
+++ back/src/database/database.py
@@ -5,8 +5,8 @@ from ..model.model import Base
 
 MYSQL_USER = os.getenv('MYSQL_USER', 'mysql')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'mysql') 
-MYSQL_HOST = 'hackathon-db-1'
-MYSQL_PORT = '3306' 
+MYSQL_HOST = os.getenv('MYSQL_HOST', 'hackathon_vol6_team21-db-1')
+MYSQL_PORT = os.getenv('MYSQL_PORT', '3306')
 MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'mysql_study')
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
