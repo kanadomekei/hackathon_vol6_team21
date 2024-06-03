c5322414d25e53dffcc844be3279e6650830e375
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-03 17:11:07 +0900

    サイドバーコンポーネントを追加

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import Login from '../../components/Login';
-import Logout from '../../components/Logout';
+import Login from '@/components/Login';
+import Logout from '@/components/Logout';
+import Sidebar from '@/components/Sidebar';
 
 export default function Home() {
 	const { data: session, status } = useSession();
