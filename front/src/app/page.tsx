c270ef14bde2e4819608334881a5243d3af8e3ec
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-05 22:03:24 +0900

    画像を一覧表示するページを作成

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,18 +1,40 @@
+"use client";
 import { Button } from "@/components/ui/button"
 import Sidebar from "@/components/Sidebar"
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+interface Post {
+  user_id: number;
+  image_url: string;
+  created_at: string;
+  caption: string;
+  id: number;
+}
 
 export default function Component() {
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/posts')
+      .then(response => response.json())
+      .then(data => setImages(data.map((item: Post) => item.image_url)))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white p-6">
-      <h1 className="text-5xl font-bold mb-8">アプリ名</h1>
-      <div className="space-y-2 mb-8">
-        <p className="text-lg">アプリの説明</p>
-        <p className="text-lg">アプリの説明</p>
-        <p className="text-lg">アプリの説明</p>
-        <p className="text-lg">アプリの説明</p>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex flex-wrap justify-center p-6 bg-white min-h-screen ml-64">
+        {images.map((src, index) => (
+          <div key={index} className="p-2">
+            <div className="relative w-48 h-48">
+              <Image src={src} alt={`image-${index}`} layout="fill" objectFit="cover" className="rounded-lg" />
+            </div>
+          </div>
+        ))}
       </div>
-      <Button className="bg-[#00BFFF] text-white">login</Button>
-	  <Sidebar />
     </div>
   )
 }
