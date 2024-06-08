01f6caa8701b49228ea672c524cfa154b74f017a
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-08 14:41:46 +0900

    goggle認証した際にデータベースに保存するように変更

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
 import Image from 'next/image';
 import Login from '@/components/Login';
 import Logout from '@/components/Logout';
@@ -7,19 +10,45 @@ import Sidebar from '@/components/Sidebar';
 
 export default function Home() {
 	const { data: session, status } = useSession();
+
+	useEffect(() => {
+		if (status === 'authenticated' && session?.user) {
+			const { name, email } = session.user;
+			axios.post(`http://localhost:8080/users/google-auth`, null, {
+				params: { username: name, email: email }
+			})
+			.then((response: AxiosResponse) => {
+				console.log('User authenticated:', response.data);
+			})
+			.catch((error: unknown) => {
+				console.error('Error during authentication:', error);
+			});
+		}
+	}, [status, session]);
+
 	return (
-		<div>
-			{status === 'authenticated' ? (
-				<div>
-					<p>セッションの期限：{session.expires}</p>
-					<p>ようこそ、{session.user?.name}さん</p>
+		<div style={{ display: 'flex' }}>
+			<Sidebar />
+			<div style={{ marginLeft: 'auto' }}>
+				{status === 'authenticated' ? (
 					<div>
-						<Logout />
+						<p>セッションの期限：{session.expires}</p>
+						<p>ようこそ、{session.user?.name}さん</p>
+						<p>メールアドレス：{session.user?.email}</p>
+						{session.user?.image && (
+							<p>
+								プロフィール画像：
+								<Image src={session.user.image} alt="プロフィール画像" width={50} height={50} />
+							</p>
+						)}
+						<div>
+							<Logout />
+						</div>
 					</div>
-				</div>
-			) : (
-				<Login />
-			)}
+				) : (
+					<Login />
+				)}
+			</div>
 		</div>
 	);
 }
