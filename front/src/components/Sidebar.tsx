44920ba487f579b37cc4b22cfcc1234e3271c656
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-09 05:03:37 +0900

    サイドバーにユーザー情報を表示するように変更

--- front/src/components/Sidebar.tsx
+++ front/src/components/Sidebar.tsx
@@ -1,7 +1,13 @@
+"use client";
 import Image from 'next/image';
 import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
+
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="fixed left-0 top-0 flex">
       <div className="w-64 h-screen bg-gray-100 p-4 flex flex-col">
@@ -10,7 +16,18 @@ export default function Sidebar() {
         <SidebarItem icon="search" label="Search" href="/with_login/search" />
         <SidebarItem icon="about" label="About" href="/with_login/about" />
         <SidebarItem icon="画像一覧" label="images" href="/with_login/images" />
-        <UserProfile name="User Name" avatarUrl="/path/to/avatar.jpg" />
+        {session && session.user ? (
+          <>
+            <UserProfile name={session.user.name || ''} avatarUrl={session.user.image || ''} />
+            <button onClick={() => signOut()} className="mt-4 p-2 bg-red-500 text-white rounded">
+              ログアウト
+            </button>
+          </>
+        ) : (
+          <Link href="/api/auth/signin">
+            ログイン
+          </Link>
+        )}
       </div>
     </div>
   )
