154f10e9c47ee56fb3a88e496aad890d1bf52dfd
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-09 04:08:44 +0900

    jsonのままレシピデータを表示するように変更

--- front/src/app/recipes/[post_id]/page.tsx
+++ front/src/app/recipes/[post_id]/page.tsx
@@ -1,40 +1,52 @@
 "use client";
-import { useEffect, useState, FC} from 'react';
+import { useEffect, useState, FC } from 'react';
 
-export default function Page({
-  params,
-}: {
-  params: { post_id : number };
-}) {
-  
-  const [recipe_id, setRecipeid] = useState("");
-  const [ingredients, setIngredients] = useState("");
-  const [instructions, setInstructions] = useState("");
+interface PageProps {
+  params: { post_id: number };
+}
+
+interface Recipe {
+  post_id: number;
+  created_at: string;
+  id: number;
+  details: {
+    ingredients: Record<string, string>;
+    recipe_name: string;
+    cooking_process: string[];
+  };
+}
+
+const Page: FC<PageProps> = ({ params }) => {
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/recipes/by_post/${params.post_id}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json'
+    const fetchRecipe = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/recipes/by_post/${params.post_id}`, {
+          headers: {
+            'accept': 'application/json',
+          },
+        });
+        const data = await response.json();
+        setRecipe(data);
+      } catch (error) {
+        console.error('Error fetching recipe:', error);
       }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); // コンソールログでデータを出力
-      setRecipeid(data.id);
-      setIngredients(data.ingredients);
-      setInstructions(data.instructions);
-    })
-    .catch(error => console.error('Error fetching recipe:', error));
+    };
+
+    fetchRecipe();
   }, [params.post_id]);
 
   return (
-    <>
-      <div className="flex flex-wrap justify-center p-6 bg-white min-h-screen ml-64">
-        this is {params.post_id}th receptだよ～<br></br>
-        材料は{ingredients}<br></br>
-        調理方法は{instructions}
-      </div>
-    </>
-  )
-}
+    <div className="flex flex-wrap justify-center p-6 bg-white min-h-screen ml-64">
+      post_id: {params.post_id}<br />
+      {recipe && (
+        <div>
+          <pre>{JSON.stringify(recipe, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Page;
