00f3f78eeeb0c8c9155c4386abbe0bb23488cf3a
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-09 00:28:54 +0900

    ログイン、ログアウトボタンを追加

--- front/src/components/Logout.tsx
+++ front/src/components/Logout.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { useSession, signOut } from 'next-auth/react';
 
 export default function Logout() {
-	const { data: session, status } = useSession();
+    const { data: session, status } = useSession();
 
-	if (status === 'authenticated') {
-		return (
-			<div>
-				<button onClick={() => signOut()}>ログアウト</button>
-			</div>
-		);
-	}
-	return null;
+    if (status === 'authenticated') {
+        return (
+            <div>
+                <button onClick={() => signOut()}
+                        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300 ease-in-out">
+                    ログアウト
+                </button>
+            </div>
+        );
+    }
+    return null;
 }
-
