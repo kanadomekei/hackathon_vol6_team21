024e56cb9d05de4ce423fd5a9f5286494192af67
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-09 00:35:54 +0900

    タイトル画面でログイン機能を追加

--- front/src/app/title/page.tsx
+++ front/src/app/title/page.tsx
@@ -1,29 +1,50 @@
-"use client";
-import React from "react";
+'use client';
+import { useSession } from 'next-auth/react';
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
+import Image from 'next/image';
+import Login from '@/components/Login';
+import Logout from '@/components/Logout';
 
-function MainComponent() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-fixed" style={{backgroundImage:'url(/images/title.png)'}}>
-      <div className="flex flex-col items-center mb-8">
-        <img
-          src="./images/logo.png"
-          alt="Chef's hat and utensils icon"
-          className="w-[400px] h-[400px] mb-4"
-        />
-      </div>
-      <div className="text-center">
-        <p className="text-4xl font-roboto text-[#000000] mb-4">
-          写真からレシピが生まれる！
-        </p>
-        <p className="text-3xl font-roboto text-[#000000] mb-4">
-          簡単クッキングで、毎日の食卓をもっと豊かに！
-        </p>
-      </div>
-      <button className="mt-8 px-6 py-2 bg-[#FAF3E3] text-[#000000] rounded-full font-roboto">
-        ログイン
-      </button>
-    </div>
-  );
+export default function Home() {
+	const { data: session, status } = useSession();
+
+	useEffect(() => {
+		if (status === 'authenticated' && session?.user) {
+			const { name, email } = session.user;
+			axios.post(`http://localhost:8080/users/google-auth`, null, {
+				params: { username: name, email: email }
+			})
+			.then((response: AxiosResponse) => {
+				console.log('User authenticated:', response.data);
+			})
+			.catch((error: unknown) => {
+				console.error('Error during authentication:', error);
+			});
+		}
+	}, [status, session]);
+
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-fixed" style={{backgroundImage:'url(/images/title.png)'}}>
+			<div className="flex flex-col items-center mb-8">
+				<Image
+					src="/images/logo.png"
+					alt="Chef's hat and utensils icon"
+					width={400}
+					height={400}
+					className="mb-4"
+				/>
+			</div>
+			<div className="text-center">
+				<p className="text-4xl font-roboto text-[#000000] mb-4">
+					写真からレシピが生まれる！
+				</p>
+				<p className="text-3xl font-roboto text-[#000000] mb-4">
+					簡単クッキングで、毎日の食卓をもっと豊かに！
+				</p>
+				{status === 'authenticated' ? <Logout /> : <Login />}
+			</div>
+		</div>
+	);
 }
-
-export default MainComponent;
