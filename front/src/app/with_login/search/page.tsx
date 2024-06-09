71dc1840ea9ecf91dd2e27c15db9f44879516ede
yamagami0413 <shanshangxiushi@gmial.com>
2024-06-09 15:02:53 +0900

    エラー修正

--- front/src/app/with_login/search/page.tsx
+++ front/src/app/with_login/search/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 
@@ -7,7 +7,7 @@ interface RecipeData {
   recipe_name: string;
   ingredients: Record<string, string>;
   cooking_process: string[];
-  image_url: string; // 追加
+  image_url: string;
 }
 
 const CombinedUpload: FC = () => {
@@ -76,7 +76,7 @@ const CombinedUpload: FC = () => {
     if (data) {
       const newRecipeData = {
         ...data,
-        image_url: selectedImage, // 追加
+        image_url: selectedImage,
       };
       setRecipeDataList(prevList => [...prevList, newRecipeData]);
     }
@@ -87,78 +87,78 @@ const CombinedUpload: FC = () => {
       className="flex flex-col items-center justify-center min-h-screen bg-no-repeat bg-cover"
       style={{ backgroundImage: `url(/images/orange.png)` }}
     >
-    <div className="flex flex-col items-center justify-center bg-[#fafafa]">
-      <title>chef's AI | Search</title>
-      <div className="w-full max-w-[900px] bg-white shadow-md rounded-lg p-8">
-        <h1 className="text-2xl font-bold text-center mb-6 font-roboto">
-          料理の画像をアップロードして原材料と調理工程を知る
-        </h1>
-        <div className="flex flex-col items-center mb-4">
-          <label className="flex flex-col items-center bg-[#f0f0f0] border border-gray-300 rounded-lg py-6 px-8 cursor-pointer w-full transition duration-200 ease-in-out hover:bg-[#e8e8e8]">
-            <i className="fas fa-cloud-upload-alt text-6xl text-gray-600 mb-4"></i>
-            <span className="font-semibold text-sm tracking-wide">
-              クリックしてファイルを選択
-            </span>
-            <input
-              type="file"
-              className="hidden"
-              name="imageUpload"
-              onChange={handleFileChange}
-            />
-          </label>
+      <div className="flex flex-col items-center justify-center bg-[#fafafa]">
+        <title>chef's AI | Search</title>
+        <div className="w-full max-w-[900px] bg-white shadow-md rounded-lg p-8">
+          <h1 className="text-2xl font-bold text-center mb-6 font-roboto">
+            料理の画像をアップロードして原材料と調理工程を知る
+          </h1>
+          <div className="flex flex-col items-center mb-4">
+            <label className="flex flex-col items-center bg-[#f0f0f0] border border-gray-300 rounded-lg py-6 px-8 cursor-pointer w-full transition duration-200 ease-in-out hover:bg-[#e8e8e8]">
+              <i className="fas fa-cloud-upload-alt text-6xl text-gray-600 mb-4"></i>
+              <span className="font-semibold text-sm tracking-wide">
+                クリックしてファイルを選択
+              </span>
+              <input
+                type="file"
+                className="hidden"
+                name="imageUpload"
+                onChange={handleFileChange}
+              />
+            </label>
+          </div>
+          {selectedImage && (
+            <div className="flex justify-center mb-4">
+              <Image
+                src={selectedImage}
+                alt="Selected preview"
+                width={300}
+                height={300}
+                className="object-cover rounded-lg"
+              />
+            </div>
+          )}
+          <button
+            className="w-full py-3 mt-4 bg-blue-500 text-white rounded-lg font-roboto font-semibold transition duration-200 ease-in-out hover:bg-blue-400"
+            onClick={handleUploadRecipe}
+            disabled={isUploading}
+          >
+            {isUploading ? 'レシピ生成中...' : 'レシピ生成'}
+          </button>
+          {recipeDataList.length > 0 && (
+            <div className="w-full mt-6">
+              {recipeDataList.map((recipeData, index) => (
+                <div key={index} className="mb-6 flex flex-col w-full">
+                  <div className="flex justify-center mb-4">
+                    <Image
+                      src={recipeData.image_url}
+                      alt="Selected preview"
+                      width={300}
+                      height={300}
+                      className="object-cover rounded-lg"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <h2 className="text-lg font-semibold text-gray-700 mt-4">レシピ名</h2>
+                    <p className="text-xl font-semibold text-gray-900">{recipeData.recipe_name}</p>
+                    <h2 className="text-lg font-semibold text-gray-700 mt-4">原材料</h2>
+                    <ul className="list-disc pl-5">
+                      {Object.entries(recipeData.ingredients).map(([ingredient, quantity]) => (
+                        <li key={ingredient} className="text-lg text-gray-900">{`${ingredient}: ${quantity}`}</li>
+                      ))}
+                    </ul>
+                    <h2 className="text-lg font-semibold text-gray-700 mt-4">調理工程</h2>
+                    <ul className="list-disc pl-5">
+                      {recipeData.cooking_process.map((step, index) => (
+                        <li key={index} className="text-lg text-gray-900">{step}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-        {selectedImage && (
-          <div className="flex justify-center mb-4">
-            <Image
-              src={selectedImage}
-              alt="Selected preview"
-              width={300}
-              height={300}
-              className="object-cover rounded-lg"
-            />
-          </div>
-        )}
-        <button
-          className="w-full py-3 mt-4 bg-blue-500 text-white rounded-lg font-roboto font-semibold transition duration-200 ease-in-out hover:bg-blue-400"
-          onClick={handleUploadRecipe}
-          disabled={isUploading}
-        >
-          {isUploading ? 'レシピ生成中...' : 'レシピ生成'}
-        </button>
-        {recipeDataList.length > 0 && (
-          <div className="w-full mt-6">
-            {recipeDataList.map((recipeData, index) => (
-              <div key={index} className="mb-6 flex flex-col w-full">
-                <div className="flex justify-center mb-4">
-                  <Image
-                    src={recipeData.image_url}
-                    alt="Selected preview"
-                    width={300}
-                    height={300}
-                    className="object-cover rounded-lg"
-                  />
-                </div>
-                <div className="w-full">
-                  <h2 className="text-lg font-semibold text-gray-700 mt-4">レシピ名</h2>
-                  <p className="text-xl font-semibold text-gray-900">{recipeData.recipe_name}</p>
-                  <h2 className="text-lg font-semibold text-gray-700 mt-4">原材料</h2>
-                  <ul className="list-disc pl-5">
-                    {Object.entries(recipeData.ingredients).map(([ingredient, quantity]) => (
-                      <li key={ingredient} className="text-lg text-gray-900">{`${ingredient}: ${quantity}`}</li>
-                    ))}
-                  </ul>
-                  <h2 className="text-lg font-semibold text-gray-700 mt-4">調理工程</h2>
-                  <ul className="list-disc pl-5">
-                    {recipeData.cooking_process.map((step, index) => (
-                      <li key={index} className="text-lg text-gray-900">{step}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            ))}
-          </div>
-
-        )}
       </div>
     </div>
   );
