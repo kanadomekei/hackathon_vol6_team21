d6338a0ebb05bf1893fc4b4283498ee324ff51af
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-09 12:13:04 +0900

    searchページを変更

--- front/src/app/with_login/search/page.tsx
+++ front/src/app/with_login/search/page.tsx
@@ -1,105 +1,172 @@
-"use client";
-import React from "react";
-import { useEffect, useState } from 'react';
+"use client"
+import React, { FC, useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
-function MainComponent() {
+interface RecipeData {
+  recipe_name: string;
+  ingredients: Record<string, string>;
+  cooking_process: string[];
+}
 
-        const [file, setFile] = useState(null);
-        const [response, setResponse] = useState(null);
-          
-        const handleFileChange = (event) => {
-              setFile(event.target.files[0]);
-            };
-          
-        const handleSubmit = async (event) => {
-              event.preventDefault();
-              if (!file) return;
-          
-              const formData = new FormData();
-              formData.append('img', file);
+const CombinedUpload: FC = () => {
+  const { data: session } = useSession();
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [recipeData, setRecipeData] = useState<RecipeData | null>(null);
+  const [isUploading, setIsUploading] = useState<boolean>(false);
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setSelectedImage(URL.createObjectURL(selectedFile));
+      setFile(selectedFile);
+    }
+  };
+
+  const uploadData = async (url: string, formData: FormData, errorMessage: string) => {
+    setIsUploading(true);
     try {
-      const res = await fetch('http://localhost:8080/ai/create_recipe', {
+      const response = await fetch(url, {
         method: 'POST',
         body: formData,
+        headers: {
+          'accept': 'application/json',
+        },
       });
-      const data = await res.json();
-      console.log(data)
-      setResponse(data);
+
+      if (!response.ok) {
+        throw new Error(errorMessage);
+      }
+
+      return await response.json();
     } catch (error) {
-      console.error('Error uploading file:', error);
+      console.error('Error:', error);
+      return null;
+    } finally {
+      setIsUploading(false);
     }
-   }
-  
-  
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <input type="file" onChange={handleFileChange} className="mb-4" />
-        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
-          アップロード
-        </button>
-      </form>
-      {response && (
-        <div className="mt-4">
-          <p>{response.recipe_name}</p>
-        </div>
-      )}
-    </div>
-  );
   };
-  export default MainComponent;
 
-//   return (
-//     <div className="flex flex-col items-center p-4">
-//       <img
-//         src="./images/saba.png"
-//         alt="Grilled mackerel dish"
-//         className="w-[300px] h-[200px] mb-4"
-//       />
+  const fetchUserId = async (email: string) => {
+    try {
+      const response = await fetch(`http://localhost:8080/users/by-email/${encodeURIComponent(email)}`);
+      if (!response.ok) {
+        throw new Error('ユーザーIDの取得に失敗しました');
+      }
+      const data = await response.json();
+      return data.user_id;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  };
 
-//       <div className="w-full max-w-[600px]">
-//         <h2 className="font-bold text-lg mb-2">材料 (1人前)</h2>
-//         <div className="border rounded p-2 mb-4">
-//           <div className="flex justify-between mb-2">
-//             <span className="font-medium">サバ (200g)</span>
-//             <span className="font-medium">1切れ</span>
-//           </div>
-//           <div className="flex justify-between mb-2">
-//             <span className="font-medium">料理酒</span>
-//             <span className="font-medium">大さじ2</span>
-//           </div>
-//           <div className="flex justify-between mb-2">
-//             <span className="font-medium">塩</span>
-//             <span className="font-medium">少々</span>
-//           </div>
-//           <div className="flex justify-between mb-2">
-//             <span className="font-medium">大葉</span>
-//             <span className="font-medium">1枚</span>
-//           </div>
-//           <div className="flex justify-between">
-//             <span className="font-medium">大根おろし</span>
-//             <span className="font-medium">適量</span>
-//           </div>
-//         </div>
+  const handleUploadRecipe = async () => {
+    if (!file) {
+      alert('ファイルを選択してください');
+      return;
+    }
 
-//         <h2 className="font-bold text-lg mb-2">作り方</h2>
-//         <ol className="list-decimal pl-6">
-//           <li className="mb-2">
-//             サバは半分に切ります。1切れは十字に切り込みを入れ、残りは約3cmに2本の切り込みを入れます。
-//           </li>
-//           <li className="mb-2">
-//             料理酒をかけて5分ほど置き、キッチンペーパーで水気を拭き取ります。
-//           </li>
-//           <li className="mb-2">
-//             アルミホイルを敷いた天板に皮目を上にしてのせ、塩をふり、オーブントースターで10分ほど焼きます。
-//           </li>
-//           <li className="mb-2">
-//             焼き目が付いたら、中央まで火が通ったら、大葉を敷いたお皿に盛り付け、大根おろしを添えて完成です。
-//           </li>
-//         </ol>
-//       </div>
-//     </div>
-//   );
+    const formData = new FormData();
+    formData.append('img', file, file.name);
 
+    const data = await uploadData('http://localhost:8080/ai/create_recipe', formData, 'レシピ生成に失敗しました');
+    setRecipeData(data);
+  };
 
+  const handleFinalUpload = async () => {
+    if (!file || !recipeData) {
+      alert('ファイルまたはレシピデータがありません');
+      return;
+    }
+
+    const email = session?.user?.email;
+    const userId = email ? await fetchUserId(email) : null;
+    if (!userId) {
+      alert('ユーザーIDの取得に失敗しました');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('caption', recipeData.recipe_name);
+
+    const responseData = await uploadData(`http://localhost:8080/posts?user_id=${userId}&caption=%E6%96%99%E7%90%86%E3%81%AE%E8%AA%AC%E6%98%8E`, formData, '最終アップロードに失敗しました');
+    if (responseData) {
+      const post_id = responseData.id;
+      const jsonResponse = await fetch(`http://localhost:8080/recipes?post_id=${post_id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(recipeData),
+      });
+
+      if (!jsonResponse.ok) {
+        throw new Error('レシピデータの送信に失敗しました');
+      }
+      alert('アップロード成功！');
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-[#fafafa]">
+      <div className="w-full max-w-[500px] bg-white shadow-md rounded-lg p-8">
+        <h1 className="text-2xl font-bold text-center mb-6 font-roboto">
+          料理の画像をアップロードして原材料と調理工程を知る
+        </h1>
+        <div className="flex flex-col items-center mb-4">
+          <label className="flex flex-col items-center bg-[#f0f0f0] border border-gray-300 rounded-lg py-6 px-8 cursor-pointer w-full transition duration-200 ease-in-out hover:bg-[#e8e8e8]">
+            <i className="fas fa-cloud-upload-alt text-6xl text-gray-600 mb-4"></i>
+            <span className="font-semibold text-sm tracking-wide">
+              クリックしてファイルを選択
+            </span>
+            <input
+              type="file"
+              className="hidden"
+              name="imageUpload"
+              onChange={handleFileChange}
+            />
+          </label>
+        </div>
+        {selectedImage && (
+          <div className="flex justify-center mb-4">
+            <Image
+              src={selectedImage}
+              alt="Selected preview"
+              width={300}
+              height={300}
+              className="object-cover rounded-lg"
+            />
+          </div>
+        )}
+        <button
+          className="w-full py-3 mt-4 bg-blue-500 text-white rounded-lg font-roboto font-semibold transition duration-200 ease-in-out hover:bg-blue-400"
+          onClick={handleUploadRecipe}
+          disabled={isUploading}
+        >
+          {isUploading ? 'レシピ生成中...' : 'レシピ生成'}
+        </button>
+        {recipeData && (
+          <div>
+            <div>
+              <h2 className="text-lg font-semibold text-gray-700 mt-4">レシピ名</h2>
+              <p className="text-xl font-semibold text-gray-900">{recipeData.recipe_name}</p>
+              <h2 className="text-lg font-semibold text-gray-700 mt-4">原材料</h2>
+              <ul className="list-disc pl-5">
+                {Object.entries(recipeData.ingredients).map(([ingredient, quantity]) => (
+                  <li key={ingredient} className="text-lg text-gray-900">{`${ingredient}: ${quantity}`}</li>
+                ))}
+              </ul>
+              <h2 className="text-lg font-semibold text-gray-700 mt-4">調理工程</h2>
+              <p className="text-xl font-semibold text-gray-900">{recipeData.cooking_process.join(', ')}</p>
+            </div>  
+          </div>
+          )}
+          </div>
+          </div>
+          );
+          }
+          
+          export default CombinedUpload;
